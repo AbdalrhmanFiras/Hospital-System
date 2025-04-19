9ec83ca1732c5f829be4348732888ea6559fbec3
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 13:08:37 +0300

    Add realations in patient

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Doctor;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Patient extends Model
 {
-    //
+    public function Doctors(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
 }
