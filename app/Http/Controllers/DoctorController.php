1a9ca604a7d6e1c9fd5186301d32234a21b40efe
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 17:56:44 +0300

    Add getPatientRecored

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class DoctorController extends Controller
 {
-    //
+
+    public function getPatientRecord($doctor_id)
+    {
+        $doctor = Doctor::find($doctor_id);
+
+        if (!$doctor) {
+            return response()->json(['message' => 'Doctor not found'], 404);
+        }
+
+        $record = $doctor->PatientRecords;
+
+        if (!$record) {
+            return response()->json(['message' => 'Doctor has no Record'], 404);
+
+        }
+        return response()->json(['message' => 'Doctor not found'], 404);
+
+    }
 }
