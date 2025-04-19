814aae0cd9787b942834e77fdbf3f3cad08f8825
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 17:08:27 +0300

    Add RegisterRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Hash;
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-
-        $request->validate([
-            'name' => 'required|string|max:225',
-            'email' => 'required|email|string',
-            'password' => 'required|string|min:7|confirmed'
-        ]);
-
 
         $user = User::create([
             'name' => $request->name,
@@ -26,4 +20,6 @@ class AuthController extends Controller
         return response()->json($user);
 
     }
+
+
 }
