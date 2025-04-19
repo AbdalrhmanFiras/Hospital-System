732171daca0f56eab1faf8db3f0b221f10d0a623
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 13:17:11 +0300

    Add realations in diagnosis

--- app/Models/Diagnosis.php
+++ app/Models/Diagnosis.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Patient;
+use App\Models\Doctor;
 
 class Diagnosis extends Model
 {
-    //
+    public function Doctors(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function Patients(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
--- app/Models/PatientRecord.php
+++ app/Models/PatientRecord.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Patient;
+use App\Models\Doctor;
+
 use App\Models\Diagnosis;
 use App\Models\prescription;
 class PatientRecord extends Model
