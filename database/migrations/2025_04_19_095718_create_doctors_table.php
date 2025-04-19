8c197f9600cbac8eb3434ef81b858db787990dbf
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 15:53:53 +0300

    implement doctors table

--- database/migrations/2025_04_19_095718_create_doctors_table.php
+++ database/migrations/2025_04_19_095718_create_doctors_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('doctors', function (Blueprint $table) {
             $table->id();
+            $table->string('Name');
+            $table->string('Specialization');
+            $table->enum('Degree', ['Bachelor', 'Master', 'Doctoral']);
+            $table->enum('Available', ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday']);
+            $table->string('patient')->nullable();
             $table->timestamps();
         });
     }
