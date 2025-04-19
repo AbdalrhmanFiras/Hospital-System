47828dda8c8c3fb59ee0efd8d8fc7d33b71bba84
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 13:17:58 +0300

    Add realations in prescription

--- app/Models/prescription.php
+++ app/Models/prescription.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Patient;
+use App\Models\Doctor;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class prescription extends Model
 {
-    //
+
+    public function Doctors(): BelongsTo
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function Patients(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
