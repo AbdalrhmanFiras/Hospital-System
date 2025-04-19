2663056b72e9bc98e40c638d56a423519cddca08
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 13:06:53 +0300

    Add realations in doctor

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Patient;
+use App\Models\PatientRecord;
+use App\Models\Diagnosis;
+use App\Models\prescription;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Doctor extends Model
 {
-    //
+    public function Patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
+
+    public function PatientRecords(): HasMany
+    {
+        return $this->hasMany(PatientRecord::class);
+    }
+
+    public function Diagnosises(): HasMany
+    {
+        return $this->hasMany(Diagnosis::class);
+    }
+
+    public function Prescriptions(): HasMany
+    {
+        return $this->hasMany(Prescription::class);
+    }
 }
