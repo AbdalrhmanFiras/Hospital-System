a2ac22ff2c200b6f6208557dcb1585d1e6774a4a
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-19 16:56:18 +0300

    Add register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
-
+use Hash;
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
 
+        $request->validate([
+            'name' => 'required|string|max:225',
+            'email' => 'required|email|string',
+            'password' => 'required|string|min:7|confirmed'
+        ]);
+
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)//dont forget to hashing the password
+        ]);
+
+        return response()->json($user);
+
+    }
 }
