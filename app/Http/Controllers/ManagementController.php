ac3312c7b28cf33b92b44109aa9244b4c9a87184
AbdalrhmanFiras <aa3819341@gmail.com>
2025-04-21 22:04:29 +0300

    Add DoctorRequest

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -2,22 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DoctorRequest;
 use App\Models\Doctor;
 use Illuminate\Http\Request;
 
 class ManagementController extends Controller
 {
-    public function DoctorSignin(Request $request)
+    public function DoctorSignin(DoctorRequest $request)
     {
-        $request->validate([
-            'name' => 'required|string',
-            'Specialization' => 'required|string',
-            'Degree' => 'required|string|in:Bachelor,Master,Doctoral',
-            'Available' => 'required|array',//
-            'Available.*' => 'required|string|in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday',
-            'phone' => 'required|string|min:10',
-            'email' => 'nullable|string|email|unique'
-        ]);
+
 
         $doctor = Doctor::create([
             'name' => $request->name,
---
+++ app/Http/Requests/DoctorRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'Specialization' => 'required|string',
+            'Degree' => 'required|string|in:Bachelor,Master,Doctoral',
+            'Available' => 'required|array',//
+            'Available.*' => 'required|string|in:Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday',
+            'phone' => 'required|string|min:10',
+            'email' => 'nullable|string|email|unique'
+        ];
+    }
+}
